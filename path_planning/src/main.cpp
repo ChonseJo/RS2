a993a4c0ad90310d3e28a9480599a67bc627c56c
Christian <christian.brischetto@gmail.com>
2025-04-29 20:03:49 +1000

    servo integration

--- path_planning/src/main.cpp
+++ path_planning/src/main.cpp
@@ -1,4 +1,5 @@
 #include "path_planning.cpp"
+#include "servo_control.cpp"
 #include <thread>
 
 int main(int argc, char * argv[])
@@ -15,12 +16,16 @@ int main(int argc, char * argv[])
 	// Create a ROS logger
 	auto const logger = rclcpp::get_logger("bottle_sorter");
 
-	// ======================================================================
-	// PATH PLANNER INIT
-	// ======================================================================
+	
 
+	// ======================================================================
+	// INIT INCLUDED FILES
+	// ======================================================================
 	// create path_planner object
 	auto path_planner = std::make_shared<PathPlanning>();
+	auto servo_control = std::make_shared<ServoControl>();
+
+
 
     // ======================================================================
     // SPINNING THE NODE AND EXECUTOR
@@ -29,6 +34,7 @@ int main(int argc, char * argv[])
 
     // Add both nodes to the executor
     executor.add_node(node);
+    executor.add_node(servo_control);
     executor.add_node(path_planner);  // Add the PathPlanning node to the executor
 
     // Spin the executor in a separate thread
@@ -40,16 +46,24 @@ int main(int argc, char * argv[])
 	path_planner->setMoveGroupPointer(&move_group_interface);
 	path_planner->setScene(); // set the planning scene
 
+
+	
     // ======================================================================
 	// project code starts here
     // ======================================================================
-	// path_planner->setJointGoal(0, -90, 0, -90, 0, 0); // home position
 
 	path_planner->init_services();
 
 	if(path_planner->getRunState() == false){
 		RCLCPP_INFO(logger, "Waiting on Run Command...");
 	}
+
+	double b1_x = 0.2;
+	double b1_y = 0.3;
+	double b2_x = 0.2;
+	double b2_y = 0.15;
+	double b3_x = 0.2;
+	double b3_y = 0.0;
 
 	while(rclcpp::ok()){
 		// Run Command
@@ -58,29 +72,33 @@ int main(int argc, char * argv[])
 			path_planner->setJointGoal(90, -90, 90, -90, -90, 0); // starting position
 
 			auto ee_pose = move_group_interface.getCurrentPose();
-			path_planner->setCartPose(ee_pose.pose.position.x, ee_pose.pose.position.y, ee_pose.pose.position.z-0.05);
-			// pickup bottles
+			auto home_x = ee_pose.pose.position.x;
+			auto home_y = ee_pose.pose.position.y;
+			auto home_z = ee_pose.pose.position.z;
 
-			path_planner->setCartPose(ee_pose.pose.position.x, ee_pose.pose.position.y,ee_pose.pose.position.z);
+			// pick up bottles
+			path_planner->setCartPose(home_x, home_y, home_z-0.05);
+			servo_control->close_servos();
+			rclcpp::sleep_for(std::chrono::milliseconds(1000)); 
+			path_planner->setCartPose(home_x, home_y, home_z);
 			
 			// *** go to bin position *** 		
-			path_planner->setCartPose(0.2,0.3,ee_pose.pose.position.z);
-			// drop bottles
+			path_planner->setCartPose(b1_x, b1_y, home_z);
+			servo_control->open_servos({1,1,1,1,1,1});
 			rclcpp::sleep_for(std::chrono::milliseconds(1000)); 
 
-			path_planner->setCartPose(0.2,0.15,ee_pose.pose.position.z);
-			// drop bottles
+			path_planner->setCartPose(b2_x, b2_y, home_z);
+			servo_control->open_servos({0,0,0,1,1,1});
 			rclcpp::sleep_for(std::chrono::milliseconds(1000)); 
 			
-			path_planner->setCartPose(0.2,0,ee_pose.pose.position.z);
-			// drop bottles
+			path_planner->setCartPose(b3_x, b3_y, home_z);
+			servo_control->open_servos({0,0,0,0,0,0});
 			rclcpp::sleep_for(std::chrono::milliseconds(1000)); 
 
 			// go home
 			path_planner->setJointGoal(90, -90, 90, -90, -90, 0); // starting position
-
-			// path_planner->setCartPose(ee_pose.position.x, ee_pose.position.y, ee_pose.position.z-0.2);
 			path_planner->resetRunState();
+			RCLCPP_INFO(logger, "Sequence completed. Bottles have been sorted");
 		}
 	}
 
@@ -92,21 +110,3 @@ int main(int argc, char * argv[])
 	rclcpp::shutdown();
 	return 0;
 }
-
-// ======================================================================
-// SUDO CODE
-//  ======================================================================
-// go to picture point
-// take picture
-// bottle detection and ML models and whatever 
-
-// move to bin 1
-// drop bottles for bin 1
-
-// move to bin 2
-// drop bottles for bin 2
-
-// move to bin 3
-// drop bottles for bin 3
-
-// go back to photo point
