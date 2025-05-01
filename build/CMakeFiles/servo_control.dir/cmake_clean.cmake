02e23df01842269c4511b7735de351361fc245ea
Christian <christian.brischetto@gmail.com>
2025-05-01 15:10:58 +1000

    gripper trys

---
+++ build/CMakeFiles/servo_control.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/servo_control.dir/src/servo_control.cpp.o"
+  "CMakeFiles/servo_control.dir/src/servo_control.cpp.o.d"
+  "libservo_control.a"
+  "libservo_control.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/servo_control.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
